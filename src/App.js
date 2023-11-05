bad25b527246cdf607a8318393d5446b721b6f86
zainulnazir <haditbutt7@gmail.com>
2023-11-05 15:04:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Currently deploying - Approbate.xyz
+          Currently deploying! - Approbate.xyz
         </p>
       </header>
     </div>
